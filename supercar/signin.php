7589d37e31df463f6bcc2668a0f57f298061d561
Anli omar <omaranli56@gmail.com>
2024-08-22 20:48:37 +0400

    added bootstrap folder to the project

--- supercar/signin.php
+++ supercar/signin.php
@@ -3,10 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"
-    >
+    <title>signin</title>
+    <link href="../bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../stylesheets/signin.css" rel="stylesheet">
     <link href="../stylesheets/navbar.css" rel="stylesheet">
   </head>
