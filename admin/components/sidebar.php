abbf07c604183bf77fad2a205a1a6d5483b649f1
Anli omar <omaranli56@gmail.com>
2024-09-11 23:31:56 +0400

    added card in dashboard.php and added php files in admin directory

--- admin/components/sidebar.php
+++ admin/components/sidebar.php
@@ -2,33 +2,75 @@
     echo"
       <div class='sidebar start-0 overflow-x-hidden fixed-top px-3' style='z-index: 1000;'>
         <div class='navbar-brand' href='/super-car/admin/admin/dashboard'>
-          <img src='../../medias/images/logos/supercar_logo_blanc.webp' alt='SuperCar logo' style='height: 50px;'>
+          <img src='../medias/images/logos/supercar_logo_blanc.webp' alt='SuperCar logo' style='height: 50px;'>
         </div>
         
         <a href='#' class='mb-3 d-flex primary-custom-btn py-b mt-3 home-btn'>
           <svg class=' pb-0' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='size-5'>
             <path fill-rule='evenodd' d='M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z' clip-rule='evenodd' />
           </svg>
-          <p class='ms-2 mt-1 pb-0'>Dashboard</p>
+          <p class='ms-2 pb-0'>Dashboard</p>
         </a>
         <div class='btn-group my-3'>
           <button class='btn btn-secondary dropdown-toggle' type='button' data-bs-toggle='dropdown' data-bs-auto-close='true' aria-expanded='false'>
             Permissions
           </button>
-          <ul class='dropdown-menu'>
-            <li><a class='dropdown-item text-dark' href='#'>Groupes</a></li>
-            <li><a class='dropdown-item text-dark' href='#'>Permissions</a></li>
-            <li><a class='dropdown-item text-dark' href='#'>Utilisateurs</a></li>
+          <ul class='dropdown-menu px-2'>
+            <li>
+              <a class='dropdown-item text-dark d-flex align-items-center py-0' href='#'>
+              <svg class='' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='size-5'>
+                  <path d='M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM1.49 15.326a.78.78 0 0 1-.358-.442 3 3 0 0 1 4.308-3.516 6.484 6.484 0 0 0-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 0 1-2.07-.655ZM16.44 15.98a4.97 4.97 0 0 0 2.07-.654.78.78 0 0 0 .357-.442 3 3 0 0 0-4.308-3.517 6.484 6.484 0 0 1 1.907 3.96 2.32 2.32 0 0 1-.026.654ZM18 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM5.304 16.19a.844.844 0 0 1-.277-.71 5 5 0 0 1 9.947 0 .843.843 0 0 1-.277.71A6.975 6.975 0 0 1 10 18a6.974 6.974 0 0 1-4.696-1.81Z' />
+              </svg>
+              <p class='mt-4 ms-2'>Groupes</p>
+              </a>
+            </li>
+            <li>
+              <a class='dropdown-item text-dark d-flex align-items-center py-0' href='#'>
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='size-5'>
+                <path fill-rule='evenodd' d='M9.661 2.237a.531.531 0 0 1 .678 0 11.947 11.947 0 0 0 7.078 2.749.5.5 0 0 1 .479.425c.069.52.104 1.05.104 1.59 0 5.162-3.26 9.563-7.834 11.256a.48.48 0 0 1-.332 0C5.26 16.564 2 12.163 2 7c0-.538.035-1.069.104-1.589a.5.5 0 0 1 .48-.425 11.947 11.947 0 0 0 7.077-2.75Zm4.196 5.954a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z' clip-rule='evenodd' />
+              </svg>
+              <p class='mt-4 ms-2'>Permissions</p>
+              </a>
+            </li>
+            <li>
+              <a class='dropdown-item text-dark d-flex align-items-center py-0' href='#'>
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='size-5'>
+                <path d='M7 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM14.5 9a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM1.615 16.428a1.224 1.224 0 0 1-.569-1.175 6.002 6.002 0 0 1 11.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 0 1 7 18a9.953 9.953 0 0 1-5.385-1.572ZM14.5 16h-.106c.07-.297.088-.611.048-.933a7.47 7.47 0 0 0-1.588-3.755 4.502 4.502 0 0 1 5.874 2.636.818.818 0 0 1-.36.98A7.465 7.465 0 0 1 14.5 16Z' />
+              </svg>
+              <p class='mt-4 ms-2'>Utilisateurs</p>
+              </a>
+            </li>
           </ul>
         </div>
         <div class='btn-group my-3'>
           <button class='btn btn-secondary dropdown-toggle' type='button' data-bs-toggle='dropdown' data-bs-auto-close='true' aria-expanded='false'>
             Voitures
           </button>
-          <ul class='dropdown-menu'>
-            <li><a class='dropdown-item text-dark' href='#'>Marques</a></li>
-            <li><a class='dropdown-item text-dark' href='#'>Modeles</a></li>
-            <li><a class='dropdown-item text-dark' href='#'>Images</a></li>
+          <ul class='dropdown-menu px-2'>
+            <li>
+              <a class='dropdown-item text-dark d-flex align-items-center py-0' href='#'>
+                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='size-5'>
+                  <path fill-rule='evenodd' d='M4.5 2A2.5 2.5 0 0 0 2 4.5v3.879a2.5 2.5 0 0 0 .732 1.767l7.5 7.5a2.5 2.5 0 0 0 3.536 0l3.878-3.878a2.5 2.5 0 0 0 0-3.536l-7.5-7.5A2.5 2.5 0 0 0 8.38 2H4.5ZM5 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z' clip-rule='evenodd' />
+                </svg>
+                <p class='mt-3 ms-2'>Marques</p>
+              </a>
+            </li>
+            <li>
+              <a class='dropdown-item text-dark d-flex align-items-center py-0' href='#'>
+                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+                  <path d='M135.2 117.4L109.1 192l293.8 0-26.1-74.6C372.3 104.6 360.2 96 346.6 96L165.4 96c-13.6 0-25.7 8.6-30.2 21.4zM39.6 196.8L74.8 96.3C88.3 57.8 124.6 32 165.4 32l181.2 0c40.8 0 77.1 25.8 90.6 64.3l35.2 100.5c23.2 9.6 39.6 32.5 39.6 59.2l0 144 0 48c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-48L96 400l0 48c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-48L0 256c0-26.7 16.4-49.6 39.6-59.2zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z'/>
+                </svg>
+                <p class='mt-4 ms-2'>Modeles</p>
+              </a>
+            </li>
+            <li>
+              <a class='dropdown-item text-dark d-flex align-items-center py-0' href='#'>
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='size-5'>
+                <path fill-rule='evenodd' d='M1 5.25A2.25 2.25 0 0 1 3.25 3h13.5A2.25 2.25 0 0 1 19 5.25v9.5A2.25 2.25 0 0 1 16.75 17H3.25A2.25 2.25 0 0 1 1 14.75v-9.5Zm1.5 5.81v3.69c0 .414.336.75.75.75h13.5a.75.75 0 0 0 .75-.75v-2.69l-2.22-2.219a.75.75 0 0 0-1.06 0l-1.91 1.909.47.47a.75.75 0 1 1-1.06 1.06L6.53 8.091a.75.75 0 0 0-1.06 0l-2.97 2.97ZM12 7a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z' clip-rule='evenodd' />
+              </svg>
+              <p class='mt-3 ms-2'>images</p>
+              </a>
+            </li>
           </ul>
         </div>
         <a href='#'>Contacts</a>
---
+++ admin/dashboard.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <!--cdn font awsome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/super-car/admin/styles/dashboard.css" rel="stylesheet">
+    <link href="/super-car/admin/components/sidebar.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row position-relative">
+        <!-- Sidebar -->
+        <?php
+            include_once('components/sidebar.php');
+        ?>
+        <!-- Main Content -->
+        <div class="position-absolute end-0 dashboard" style="z-index: 10;">
+            <div class="dashboard-header mt-2 h-auto py-3 px-3 d-flex justify-content-flex-between">
+                <div class="d-flex justify-content-between align-items-center w-25 px-2">
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='size-5'>
+                        <path fill-rule='evenodd' d='M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75Zm0 10.5a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1-.75-.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Z' clip-rule='evenodd' />
+                    </svg>
+                    <h4>Bienvenue, John!</h4>
+                </div>
+                <div class="d-flex justify-content-end align-items-center w-75 pe-3">
+                    <div class="d-flex align-items-center">
+                        <svg class="mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path d="M13.024 9.25c.47 0 .827-.433.637-.863a4 4 0 0 0-4.094-2.364c-.468.05-.665.576-.43.984l1.08 1.868a.75.75 0 0 0 .649.375h2.158ZM7.84 7.758c-.236-.408-.79-.5-1.068-.12A3.982 3.982 0 0 0 6 10c0 .884.287 1.7.772 2.363.278.38.832.287 1.068-.12l1.078-1.868a.75.75 0 0 0 0-.75L7.839 7.758ZM9.138 12.993c-.235.408-.039.934.43.984a4 4 0 0 0 4.094-2.364c.19-.43-.168-.863-.638-.863h-2.158a.75.75 0 0 0-.65.375l-1.078 1.868Z" />
+                            <path fill-rule="evenodd" d="m14.13 4.347.644-1.117a.75.75 0 0 0-1.299-.75l-.644 1.116a6.954 6.954 0 0 0-2.081-.556V1.75a.75.75 0 0 0-1.5 0v1.29a6.954 6.954 0 0 0-2.081.556L6.525 2.48a.75.75 0 1 0-1.3.75l.645 1.117A7.04 7.04 0 0 0 4.347 5.87L3.23 5.225a.75.75 0 1 0-.75 1.3l1.116.644A6.954 6.954 0 0 0 3.04 9.25H1.75a.75.75 0 0 0 0 1.5h1.29c.078.733.27 1.433.556 2.081l-1.116.645a.75.75 0 1 0 .75 1.298l1.117-.644a7.04 7.04 0 0 0 1.523 1.523l-.645 1.117a.75.75 0 1 0 1.3.75l.644-1.116a6.954 6.954 0 0 0 2.081.556v1.29a.75.75 0 0 0 1.5 0v-1.29a6.954 6.954 0 0 0 2.081-.556l.645 1.116a.75.75 0 0 0 1.299-.75l-.645-1.117a7.042 7.042 0 0 0 1.523-1.523l1.117.644a.75.75 0 0 0 .75-1.298l-1.116-.645a6.954 6.954 0 0 0 .556-2.081h1.29a.75.75 0 0 0 0-1.5h-1.29a6.954 6.954 0 0 0-.556-2.081l1.116-.644a.75.75 0 0 0-.75-1.3l-1.117.645a7.04 7.04 0 0 0-1.524-1.523ZM10 4.5a5.475 5.475 0 0 0-2.781.754A5.527 5.527 0 0 0 5.22 7.277 5.475 5.475 0 0 0 4.5 10a5.475 5.475 0 0 0 .752 2.777 5.527 5.527 0 0 0 2.028 2.004c.802.458 1.73.719 2.72.719a5.474 5.474 0 0 0 2.78-.753 5.527 5.527 0 0 0 2.001-2.027c.458-.802.719-1.73.719-2.72a5.475 5.475 0 0 0-.753-2.78 5.528 5.528 0 0 0-2.028-2.002A5.475 5.475 0 0 0 10 4.5Z" clip-rule="evenodd" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-5.5-2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10 12a5.99 5.99 0 0 0-4.793 2.39A6.483 6.483 0 0 0 10 16.5a6.483 6.483 0 0 0 4.793-2.11A5.99 5.99 0 0 0 10 12Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Finance Section -->
+            <div class="row mt-5 d-flex justify-content-center">
+                <div class="col-3 d-flex flex-column border rounded-4 px-3 py-2 shadow mx-5">
+                    <div class="d-flex justify-content-between mb-0">
+                        <div class="icon" style="height:65px; width: 65px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM1.49 15.326a.78.78 0 0 1-.358-.442 3 3 0 0 1 4.308-3.516 6.484 6.484 0 0 0-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 0 1-2.07-.655ZM16.44 15.98a4.97 4.97 0 0 0 2.07-.654.78.78 0 0 0 .357-.442 3 3 0 0 0-4.308-3.517 6.484 6.484 0 0 1 1.907 3.96 2.32 2.32 0 0 1-.026.654ZM18 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM5.304 16.19a.844.844 0 0 1-.277-.71 5 5 0 0 1 9.947 0 .843.843 0 0 1-.277.71A6.975 6.975 0 0 1 10 18a6.974 6.974 0 0 1-4.696-1.81Z" />
+                            </svg>            
+                        </div>
+                        <div class="numbers d-flex flex-column align-items-end pb-2">
+                            <p class="mb-0" style="font-size: 18px;">Utilisateurs</p>
+                            <h4 class="mt-0 ms-1">100</h4>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <hr class="py-0 my-0 d-flex justify-content-center" >
+                    </div>
+                    <div class="mt-3">
+                        <p><strong class="text-success">+55%</strong> than last week</p>
+                    </div>
+                </div>
+                <div class="col-3 d-flex flex-column border rounded-4 px-3 py-2 shadow mx-5">
+                    <div class="d-flex justify-content-between mb-0">
+                        <div class="icon" style="height:65px; width: 65px;">
+                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+                                <path d='M135.2 117.4L109.1 192l293.8 0-26.1-74.6C372.3 104.6 360.2 96 346.6 96L165.4 96c-13.6 0-25.7 8.6-30.2 21.4zM39.6 196.8L74.8 96.3C88.3 57.8 124.6 32 165.4 32l181.2 0c40.8 0 77.1 25.8 90.6 64.3l35.2 100.5c23.2 9.6 39.6 32.5 39.6 59.2l0 144 0 48c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-48L96 400l0 48c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-48L0 256c0-26.7 16.4-49.6 39.6-59.2zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z'/>
+                            </svg>          
+                        </div>
+                        <div class="numbers d-flex flex-column align-items-end pb-2">
+                            <p class="mb-0" style="font-size: 18px;">Modèles voitures</p>
+                            <h4 class="mt-0 ms-1">100</h4>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <hr class="py-0 my-0 d-flex justify-content-center" >
+                    </div>
+                    <div class="mt-3">
+                        <p><strong class="text-success">+55%</strong> than last week</p>
+                    </div>
+                </div>
+                <div class="col-3 d-flex flex-column border rounded-4 px-3 py-2 shadow mx-5">
+                    <div class="d-flex justify-content-between mb-0">
+                        <div class="icon" style="height:65px; width: 65px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                        </div>
+                        <div class="numbers d-flex flex-column align-items-end pb-2">
+                            <p class="mb-0" style="font-size: 18px;">Visites</p>
+                            <h4 class="mt-0 ms-1">100</h4>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <hr class="py-0 my-0 d-flex justify-content-center" >
+                    </div>
+                    <div class="mt-3">
+                        <p><strong class="text-success">+55%</strong> than last week</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Enrolled Courses -->
+            <div class="row mt-4">
+                <div class="col-md-6">
+                    <div class="course-card">
+                        <h5>Object Oriented Programming</h5>
+                        <a href="#" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="course-card">
+                        <h5>Fundamentals of Database Systems</h5>
+                        <a href="#" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Instructor Section & Notice -->
+            <div class="row mt-4">
+                <div class="col-md-4">
+                    <h5>Course Instructors</h5>
+                    <div class="d-flex">
+                        <img src="https://via.placeholder.com/50" class="avatar me-2" alt="Instructor 1">
+                        <img src="https://via.placeholder.com/50" class="avatar me-2" alt="Instructor 2">
+                        <img src="https://via.placeholder.com/50" class="avatar" alt="Instructor 3">
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="notice">
+                        <h5>Daily Notice</h5>
+                        <p><strong>Prelim payment due:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p><strong>Exam schedule:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
