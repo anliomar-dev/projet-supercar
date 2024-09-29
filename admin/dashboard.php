26e36ec322f797a98ceaa6bcf87367de1bda227b
Anli omar <omaranli56@gmail.com>
2024-09-29 23:55:56 +0400

    aded recents actions and calender in dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -14,36 +14,11 @@
     <script src="js/dashboard.js" type="module" defer></script>
     <script src="js/sidebar_navbar.js" type="module" defer></script>
     <link href="/super-car/admin/components/sidebar.css" rel="stylesheet">
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script type="text/javascript">
-        google.charts.load('current', { packages: ['corechart'] });
-        google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-            var data = google.visualization.arrayToDataTable([
-                ['essais', 'essais par marque'],
-                ['Ferrari', 8],
-                ['Jeep', 2],
-                ['Mercedes-Benz', 12],
-                ['Porsche', 4],
-            ]);
-
-            var options = {
-                title: 'Demandes d\'esssai par marques',
-                is3D: true,
-            };
-
-            var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-            chart.draw(data, options);
-        }
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar/index.global.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.7/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.7/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@6.1.7/main.min.js"></script>
     <style>
-        #myChart {
-            max-width: 600px;
-            height: 200px;
-            margin: 0 auto; /* Centrer le canvas */
-        }
         .close-sidebar{
             display: none;
         }
@@ -79,7 +54,7 @@
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center">
-                        <hr class="py-0 my-0 d-flex justify-content-center" >
+                        <hr class="py-0 my-0 d-flex justify-content-center stat-cards_line" >
                     </div>
                     <div class="mt-3">
                         <p><strong class="text-success">+ 100</strong> utilisateurs inscrits</p>
@@ -98,7 +73,7 @@
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center">
-                        <hr class="py-0 my-0 d-flex justify-content-center" >
+                        <hr class="py-0 my-0 d-flex justify-content-center stat-cards_line" >
                     </div>
                     <div class="mt-3">
                         <p><strong class="text-success">+24</strong> Modèles</p>
@@ -117,7 +92,7 @@
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center">
-                        <hr class="py-0 my-0 d-flex justify-content-center" >
+                        <hr class="py-0 my-0 d-flex justify-content-center stat-cards_line" >
                     </div>
                     <div class="mt-3">
                         <p><strong class="text-success">+24</strong> Modèles</p>
@@ -137,71 +112,134 @@
                         </div>
                     </div>
                     <div class="col-12 d-flex justify-content-center">
-                        <hr class="py-0 my-0 d-flex justify-content-center" >
+                        <hr class="py-0 my-0 d-flex justify-content-center stat-cards_line" >
                     </div>
                     <div class="mt-3">
                         <p><strong class="text-success">+55%</strong> que la semainde dernière</p>
                     </div>
                 </div>
             </div>
-            <div class="row mt-3">
-                <div id="piechart_3d" class="bg-transparent rounded-3 ms-3" style="width: auto; height: 300px;"></div>
-                <canvas id="myChart" class=""></canvas>
-            </div>
-            <!-- Enrolled Courses -->
-            <div class="row mt-4">
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>Demandes d'essais</h5>
-                        <a href="#" class="btn btn-primary">View</a>
+            <div class="row px-4">
+                <div class="timeline col-md-4 mx-auto">
+                    <h5 class="text-center my-3">Actions recentes</h5>
+                    <hr>
+                    <div class="timeline-item h-auto">
+                        <div class="action-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z" clip-rule="evenodd" />
+                        </svg>              
+                        </div>
+                        <div class="timeline-card p-3">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between">
+                                    <span class="user-name">omar</span>
+                                    <span class="time-text"><i class="fas fa-clock"></i> il y'a 5 min</span>
+                                </div>
+                                <hr class="my-2">
+                                <p class="mb-1">Table : <strong>utilisateur</strong></p>
+                                <p class="mb-1">Action : <strong>ajouter</strong> | Id : <span class="id-value">29</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-item h-auto">
+                        <div class="action-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z" clip-rule="evenodd" />
+                        </svg>              
+                        </div>
+                        <div class="timeline-card p-3">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between">
+                                    <span class="user-name">omar</span>
+                                    <span class="time-text"><i class="fas fa-clock"></i> il y'a 5 min</span>
+                                </div>
+                                <hr class="my-2">
+                                <p class="mb-1">Table : <strong>utilisateur</strong></p>
+                                <p class="mb-1">Action : <strong>ajouter</strong> | Id : <span class="id-value">29</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-item h-auto">
+                        <div class="action-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z" clip-rule="evenodd" />
+                        </svg>              
+                        </div>
+                        <div class="timeline-card p-3">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between">
+                                    <span class="user-name">omar</span>
+                                    <span class="time-text"><i class="fas fa-clock"></i> il y'a 5 min</span>
+                                </div>
+                                <hr class="my-2">
+                                <p class="mb-1">Table : <strong>utilisateur</strong></p>
+                                <p class="mb-1">Action : <strong>ajouter</strong> | Id : <span class="id-value">29</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <div class="action-icon border">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/>
+                        </svg>            
+                        </div>
+                        <div class="timeline-card p-3">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between">
+                                    <span class="user-name">julien</span>
+                                    <span class="time-text"><i class="fas fa-clock"></i> il y'a 5 min</span>
+                                </div>
+                                <hr class="my-2">
+                                <p class="mb-1">Table : <strong>modele</strong></p>
+                                <p class="mb-1">Action : <strong>modifier</strong> | Id : <span class="id-value">12</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <div class="action-icon border">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/>
+                        </svg>            
+                        </div>
+                        <div class="timeline-card p-3">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between">
+                                    <span class="user-name">julien</span>
+                                    <span class="time-text"><i class="fas fa-clock"></i> il y'a 5 min</span>
+                                </div>
+                                <hr class="my-2">
+                                <p class="mb-1">Table : <strong>modele</strong></p>
+                                <p class="mb-1">Action : <strong>modifier</strong> | Id : <span class="id-value">12</span></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>evennements</h5>
-                        <a href="#" class="btn btn-primary">View</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>Demandes d'essais</h5>
-                        <a href="#" class="btn btn-primary">View</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>evennements</h5>
-                        <a href="#" class="btn btn-primary">View</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>Demandes d'essais</h5>
-                        <a href="#" class="btn btn-primary">View</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>evennements</h5>
-                        <a href="#" class="btn btn-primary">View</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>Demandes d'essais</h5>
-                        <a href="#" class="btn btn-primary">View</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="course-card">
-                        <h5>evennements</h5>
-                        <a href="#" class="btn btn-primary">View</a>
-                    </div>
+                <div class="calander_container col-md-8 mx-auto">
+                    <h5 class="text-center my-3">Calendrier des essais</h5>
+                    <hr>
+                    <div id="calendar"></div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+        document.addEventListener("DOMContentLoaded", async function () {
+            const todayDate = new Date()
+            var calendarEl = document.getElementById("calendar");
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: "dayGridMonth",
+            headerToolbar: {
+                start: "dayGridMonth,timeGridWeek,timeGridDay,list today",
+                center: "title",
+                end: "prev,next",
+            },
+            
+            });
+
+            calendar.render();
+        });
+    </script>
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
