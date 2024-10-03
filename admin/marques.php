87be575defa511ebb792e7f755099df0e442ee06
Anli omar <omaranli56@gmail.com>
2024-10-03 10:03:05 +0400

    added authentication verification in all page

--- admin/marques.php
+++ admin/marques.php
@@ -1,3 +1,10 @@
+<?php
+    // start new session if there is not a session
+    include_once('../php/utils.php');
+    $LOGIN_URL = "/super-car/admin/login";
+    $SESSION_EXPIRED_URL = "/super-car/admin/session_expired";
+    is_user_authenticated(2, $LOGIN_URL, $SESSION_EXPIRED_URL);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
